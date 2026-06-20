ff6127d40448cc44fd38f7771c5ec26401dd9b31
Daniel Elskamp <ich@dginx.de>
2026-06-20 02:34:58 +0200

    feat(i18n): translate sidebar and user menu chrome

---
+++ lang/nl/common.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'nav' => [
+        'dashboard' => 'Dashboard',
+        'links' => 'Links',
+        'domains' => 'Domeinen',
+        'qrcodes' => 'QR-codes',
+        'pixels' => 'Pixels',
+        'statistics' => 'Statistieken',
+        'activity' => 'Activiteit',
+        'team' => 'Team',
+    ],
+    'user_menu' => [
+        'admin' => 'Beheer',
+        'profile' => 'Profiel',
+        'logout' => 'Uitloggen',
+    ],
+    'language' => [
+        'label' => 'Taal',
+    ],
+    'actions' => [
+        'save' => 'Opslaan',
+        'cancel' => 'Annuleren',
+        'delete' => 'Verwijderen',
+        'edit' => 'Bewerken',
+        'create' => 'Aanmaken',
+        'confirm' => 'Bevestigen',
+        'close' => 'Sluiten',
+        'search' => 'Zoeken',
+    ],
+];
